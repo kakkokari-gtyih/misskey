ee5c88fd6c73eb3068411fb679327941c2b12959
kakkokari-gtyih <67428053+kakkokari-gtyih@users.noreply.github.com>
2026-07-24 13:23:04 +0900

    enhance: more tree shaking

--- packages/backend/src/misc/node-av-log.ts
+++ packages/backend/src/misc/node-av-log.ts
@@ -3,7 +3,8 @@
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 
-import { AV_LOG_ERROR, Log } from 'node-av';
+import { Log } from 'node-av/lib';
+import { AV_LOG_ERROR } from 'node-av/constants';
 
 // libav 自体のログ出力を抑制する
 // node-av を使うモジュールから副作用 import する必要がある
